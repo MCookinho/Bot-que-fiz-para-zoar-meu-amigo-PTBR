80af84cf829977a14a2798b1af6a013679bdb36d
MCookinho <148594362+MisterCookinho@users.noreply.github.com>
2024-05-20 18:25:24 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import os
 api_key = os.environ['TWITTER_API_KEY']
 api_secret = os.environ['TWITTER_API_SECRET']
 access_token = os.environ['TWITTER_ACCESS_TOKEN']
-access_secret = os.environ['TWITTER_ACCESS_SECRET']
+access_token_secret = os.environ['TWITTER_ACCESS_SECRET']
 
 def get_twitter_conn_v1(api_key, api_secret, access_token, access_token_secret) -> tweepy.API:
     """Get twitter conn 1.1"""
